da54f012f238e3e3f7434517331f19fee74b15e7
Henrique-93 <42453235+Henrique-93@users.noreply.github.com>
2018-09-25 18:19:54 -0300

    Add files via upload

---
+++ TESTE.R
@@ -0,0 +1,5 @@
+install.packages("urca")   
+library("urca") 
+library(readxl)
+interdaay <-GS read_excel("C:/Econometria/interdaay.xls",col_types = c("date", "numeric", "numeric", "numeric")) 
+                
