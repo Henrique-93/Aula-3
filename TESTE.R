526599c8de6029727bd56216faeb56a646d556a9
Henrique-93 <42453235+Henrique-93@users.noreply.github.com>
2018-09-26 15:03:40 -0300

    Delete TESTE.R

--- TESTE.R
+++
@@ -1,5 +0,0 @@
-install.packages("urca")   
-library("urca") 
-library(readxl)
-interdaay <-GS read_excel("C:/Econometria/interdaay.xls",col_types = c("date", "numeric", "numeric", "numeric")) 
-                
